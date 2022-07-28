281187407742bfd9752cbd06449e111935e7baf6
SergeiKsv <krasnoperov.sergey@gmail.com>
2022-07-28 18:05:30 +0600

    redux toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
-import { combineReducers, createStore } from "redux";
 import { authReducer } from "./redux/reducers/authReducer";
 import { Provider } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({
-  reduxAuth: authReducer
+
+const store = configureStore({
+    reducer:authReducer
 });
 
-const store = createStore(rootReducer);
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
